cf68a0e248aad61e9a025dfa222fd1fa5ae7af70
Chingupingu30 <teodavid2994@gmail.com>
2026-05-01 17:29:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
-    <!-- Histats.com  START  (aync)-->
+   <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,5023961,4,0,0,0,00010000']);
+_Hasync.push(['Histats.start', '1,5023963,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
@@ -12,7 +12,7 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5023961&101" alt="php hit counter" border="0"></a></noscript>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5023963&101" alt="unique visitors counter" border="0"></a></noscript>
 <!-- Histats.com  END  -->
     <meta charset="utf-8">
     <title>Electro - Electronics Website Template</title>
